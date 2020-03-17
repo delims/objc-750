61475f802d86845cc49c27fabc2a79f9803e04f0
delims <delims.com>
2020-03-17 11:23:53 +0800

    2020-03-17

--- objc4-750/myTest/main.mm
+++ objc4-750/myTest/main.mm
@@ -8,29 +8,19 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 #import <malloc/malloc.h>
+#import <objc/message.h>
 #import "test_c.hpp"
-
-
 
 int sum(int num, ...)
 {
     va_list valist;
-    
     int sum = 0;
-    
     va_start(valist, num);
-    
     for (int i = 0; i < num; i ++) {
-        
         sum += va_arg(valist, int);
     }
-    
     va_end(valist);
-    
     return sum;
-    
-    
-    
 }
 
 
@@ -48,27 +38,48 @@ void bubble_sort(int arr[],int len)
     }
 }
 
+NSObject *(send2)(NSString *str)
+{
+    printf("hehlo");
+    return nil;
+}
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        run();
+//        run();
         
         printf("\n");
         printf("\n");
         printf("\n");
 
-        return 0;
 //        NSObject *obj = [[NSObject alloc] init];
-        //获得NSObject实例对象的成员变量所占用的大小 >> 8
+//      //  获得NSObject实例对象的成员变量所占用的大小 >> 8
 //        NSLog(@"%zd", class_getInstanceSize([NSObject class]));
+        
+        
+        NSObject*uobject = ((NSObject *(*)(id, SEL))(objc_msgSend))(objc_getClass("NSObject"), sel_registerName("alloc"));
+        NSObject *object = ((NSObject *(*)(id, SEL))objc_msgSend)(uobject, sel_registerName("init"));
 //
 //        //获得obj指针所指向内存的大小 >> 16
 //        //malloc_size(const void *ptr):Returns size of given ptr
 //        NSLog(@"%zd", malloc_size((__bridge const void *)obj));
         
+//        NSObject *(*send)(NSString *string);
+//
+//        send = &send2;
+//
+//        send(@"");
         
-//        NSLog(@"%s",__TIME__);
+        NSString *a_string = @"";
+        NSString *b_string = [@"" mutableCopy];
+        NSString *c_string = [NSString stringWithFormat:@"%@",@"123"];
+
+        NSLog(@"%lx--%p--%p",(NSUInteger)a_string,b_string,c_string);
+        NSLog(@"%p--%p--%p",a_string,b_string,c_string);
+        NSLog(@"%p--%p--%p",&a_string,&b_string,&c_string);
+
 //
 //        while (1) {
 //            printf("%s\n",__TIME__);
@@ -80,7 +91,7 @@ int main(int argc, const char * argv[]) {
 //
 //        
 ////        NSLog(@"%p--%lu",des,sizeof(des));
-//        
+//
 //        
 //        if (des == NULL) {
 //            printf("malloc failure");
